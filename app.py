4d99dcae2142e40808833153cd952a8501d6b24c
sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-09-28 15:12:35 -0300

    Creando modelo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from models import db, User, Post
+from models import db, Seller, Buyer
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -10,19 +10,42 @@ app.config['DEBUG'] = True
 db.init_app(app)
 Migrate(app, db)
 
-@app.route ("/user", methods=["GET", "POST"])
-def user():
+@app.route ("/seller", methods=["GET", "POST"])
+def seller():
     if request.method == "GET":
-        user = User.query.get(1)
+        seller = Seller.query.get(1)
         return jsonify(user.serialize()), 200
     else:
-        user = User()
-        user.name = request.json.get("name")
-        user.password = request.json.get("password")
-        user.email = request.json.get("email")
-        user.isActive = request.json.get("isActive")
+        seller = Seller()
+        seller.firstname = request.json.get("firstname")
+        seller.lastname = request.json.get("lastname")
+        seller.rut = request.json.get("rut")
+        seller.password = request.json.get("password")
+        seller.email = request.json.get("email")
+        seller.link = request.json.get("link")
+        
 
-        db.session.add(user)
+        db.session.add(seller)
+        db.session.commit()
+
+    return jsonify(user.serialize()), 200
+
+@app.route ("/buyer", methods=["GET", "POST"])
+def buyerr():
+    if request.method == "GET":
+        buyer = Buyer.query.get(1)
+        return jsonify(user.serialize()), 200
+    else:
+        buyer = Buyer()
+        buyer.firstname = request.json.get("firstname")
+        buyer.lastname = request.json.get("lastname")
+        buyer.rut = request.json.get("rut")
+        buyer.password = request.json.get("password")
+        buyer.email = request.json.get("email")
+        
+        
+
+        db.session.add(buyer)
         db.session.commit()
 
     return jsonify(user.serialize()), 200
--- models.py
+++ models.py
@@ -1,50 +1,61 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'user'
+class Seller(db.Model):
+    __tablename__ = 'seller'
     id= db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(10), nullable=False)
-    email = db.Column(db.String(30), nullable=False)
-    isActive = db.Column(db.Boolean, default=False)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    rut = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    link = db.Column(db.String(100), nullable=False)
+    
 
     def __repr__(self):
-        return "<User %r>" % self.id
+        return "<Seller %r>" % self.id
 
     def serialize(self):
         return {
             'id': self.id,
-            'name': self.name,
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'rut': self.rut,
             'password': self.password,
             'email': self.email,
-            'isActive': self.isActive
+            'link': self.link
         }
-    def serialize_just_username(self):
+    def serialize_just_name(self):
         return {
             'id': self.id,
-            'name': self.name
+            'firstname': self.firstname,
+            'lastname': self.lastname
         }
 
-class Post(db.Model):
-    __tablename__ = 'post'
-    id= db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    name = db.Column(db.String(250), nullable=False)
-    description = db.Column(db.String(250), nullable=False)
-    user = db.relationship("User", backref=db.backref("user", lazy = True))
+class Buyer(db.Model):
+    __tablename__ = 'buyer'
+        id= db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    rut = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return "<Post %r>" % self.id
 
     def serialize(self):
         return {
-            'user_id': self.user_id,
             'id': self.id,
-            'name': self.name,
-            'description': self.description
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'rut': self.rut,
+            'password': self.password,
+            'email': self.email
         }
-    def serialize_just_userid(self):
+    def serialize_just_name(self):
         return {
-            'user_id': self.user_id 
+            'id': self.id,
+            'firstname': self.firstname,
+            'lastname': self.lastname
         }
