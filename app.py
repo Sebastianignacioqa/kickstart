7e9175df996c992e7be482e7563e157d261b89d7
sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-10-21 23:47:21 -0300

    Agregado multiples archivos al submit

--- app.py
+++ app.py
@@ -145,11 +145,11 @@ def index():
     return "El documento fue adjunto satisfactoriamente"
 
 @app.route('/post', methods=['POST'])
-def upload_file():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
-    return "Hello world"
+def upload_files():
+    for uploaded_file in request.files.getlist('file'):
+        if uploaded_file.filename != '':
+            uploaded_file.save(uploaded_file.filename)
+    return "El documento fue adjunto satisfactoriamente"
 
 if __name__== "__main__":
     app.run(host='localhost', port = 8080)
