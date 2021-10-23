569c9149ee4e117f6e1747305958df82765d7adb
Isaac <icornejomoraga@gmail.com>
2021-10-23 18:58:43 -0300

    actualizacion de tablas

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from models import db, Seller, Buyer, Balance
+from models import db, Seller, Buyer, Sale, Product, Favorite, Payment, Dispatch, Balance
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:6kjqxSDJ44!w@localhost:5432/kickstart'
@@ -9,8 +10,39 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 db.init_app(app)
 Migrate(app, db)
+CORS(app)
 
-@app.route ("/seller", methods=["GET", "POST"])
+@app.route ("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        seller = Seller.query.get(1)
+        return jsonify(seller.serialize()), 200
+    else:
+        seller = Seller()
+        seller.rut = request.json.get("rut")
+        seller.password = request.json.get("password")
+        
+        return jsonify(seller.serialize_just_login()), 200
+
+
+@app.route ("/categorias", methods=["GET", "POST"])
+def artesanias():
+    if request.method == "POST":
+        categoria = request.json.get("categoria")
+        if categoria is None:
+            return jsonify("Categoría no válida"), 400
+        else:
+            arreglo=[]
+            categorias = Seller.query.filter_by(category=categoria).all()
+            if categorias is None:
+                return jsonify("No existen tiendas"), 200
+            for category in categorias:
+                arreglo.append(category.storename)
+            return jsonify(arreglo), 200
+
+            
+
+@app.route ("/registrotienda", methods=["GET", "POST"])
 def seller():
     if request.method == "GET":
         seller = Seller.query.get(1)
@@ -20,9 +52,13 @@ def seller():
         seller.firstname = request.json.get("firstname")
         seller.lastname = request.json.get("lastname")
         seller.rut = request.json.get("rut")
-        seller.password = request.json.get("password")
         seller.email = request.json.get("email")
+        seller.password = request.json.get("password")
+        seller.address = request.json.get("address")
+        seller.phonenumber = request.json.get("phonenumber")
+        seller.storename = request.json.get("storename")       
         seller.link = request.json.get("link")
+        seller.category = request.json.get("category")
         
 
         db.session.add(seller)
@@ -51,6 +87,101 @@ def buyer():
     return jsonify(user.serialize()), 200
 
 
+@app.route ("/sale", methods=["GET", "POST"])
+def sale():
+    if request.method == "GET":
+        sale = Sale.query.get(1)
+        return jsonify(sale.serialize()), 200
+    else:
+        sale = Sale()
+        sale.sellerID = request.json.get("sellerID")
+        sale.buyerID = request.json.get("buyerID")
+      
+        db.session.add(sale)
+        db.session.commit()
+
+        return jsonify(sale.serialize()), 200
+
+
+@app.route ("/product", methods=["GET", "POST", "PUT"])
+def product():
+    if request.method == "GET":
+        product = Product.query.get(1)
+        return jsonify(product.serialize()), 200
+        
+    if request.method == "PUT":
+        product = Product.query.get(1)
+        product.item_title = ""
+        product.item_photo = ""
+        product.item_description = ""
+        product.item_stock = ""
+        product.item_price = ""
+
+        db.session.commit()
+    else:
+        product = Product()
+        product.sellerID = request.json.get("sellerID")
+        product.storename = request.json.get("storename")
+        product.item_title = request.json.get("item_title")
+        product.item_photo = request.json.get("item_photo")
+        product.item_description = request.json.get("item_description")
+        product.item_stock = request.json.get("item_stock")
+        product.item_price = request.json.get("item_price")
+
+        db.session.add(product)
+        db.session.commit()
+
+        return jsonify(product.serialize()), 200
+
+
+@app.route ("/favorite", methods=["GET", "POST"])
+def favorite():
+    if request.method == "GET":
+        favorite = Favorite.query.get(1)
+        return jsonify(favorite.serialize()), 200
+    else:
+        favorite = Favorite()
+        favorite.buyerID = request.json.get("buyerID")
+        favorite.sellerID = request.json.get("sellerID")
+        favorite.storename = request.json.get("storename")    
+
+        db.session.add(favorite)
+        db.session.commit()
+
+        return jsonify(favorite.serialize()), 200
+
+@app.route ("/payment", methods=["GET", "POST"])
+def payment():
+    if request.method == "GET":
+        payment = Payment.query.get(1)
+        return jsonify(payment.serialize()), 200
+    else:
+        payment = Payment()
+        payment.buyerID = request.json.get("buyerID")
+        payment.debit = request.json.get("debit")
+        payment.credit = request.json.get("credit")
+        payment.transfer = request.json.get("transfer")     
+
+        db.session.add(payment)
+        db.session.commit()
+
+        return jsonify(payment.serialize()), 200
+
+@app.route ("/dispatch", methods=["GET", "POST"])
+def dispatch():
+    if request.method == "GET":
+        dispatch = Dispatch.query.get(1)
+        return jsonify(dispatch.serialize()), 200
+    else:
+        dispatch = Dispatch()
+        dispatch.sellerID = request.json.get("sellerID")
+        dispatch.in_address = request.json.get("in_address")
+        dispatch.delivery = request.json.get("delivery")   
+
+        db.session.add(dispatch)
+        db.session.commit()
+
+        return jsonify(dispatch.serialize()), 200
 
 
 @app.route ("/balance", methods=["GET", "POST", "PUT"])
@@ -69,9 +200,11 @@ def balance():
 
     else:
         balance = Balance()
-        current_balance = request.json.get("current_balance")
-        last_deposit = request.json.get("last_deposit")
-        last_withdraw = request.json.get("last_withdraw")
+        balance.sellerID = request.json.get("sellerID")
+        balance.storename = request.json.get("storename")
+        balance.current_balance = request.json.get("current_balance")
+        balance.last_deposit = request.json.get("last_deposit")
+        balance.last_withdraw = request.json.get("last_withdraw")   
         
         
         
--- models.py
+++ models.py
@@ -6,10 +6,18 @@ class Seller(db.Model):
     id= db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
-    rut = db.Column(db.String(10), nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    rut = db.Column(db.String(12), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+    phonenumber = db.Column(db.String(15), nullable=False)
+    storename = db.Column(db.String(30), nullable=False)   
     link = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(20), nullable=False)
+    product = db.relationship("Product", backref=db.backref("seller", lazy = True))
+    favorite = db.relationship("Favorite", backref=db.backref("seller", lazy = True))
+    dispatch = db.relationship("Dispatch", backref=db.backref("seller", lazy = True))
+    balance = db.relationship("Balance", backref=db.backref("seller", lazy = True))
     
 
     def __repr__(self):
@@ -21,15 +29,20 @@ class Seller(db.Model):
             'firstname': self.firstname,
             'lastname': self.lastname,
             'rut': self.rut,
-            'password': self.password,
             'email': self.email,
-            'link': self.link
+            'address': self.address,
+            'phonenumber': self.phonenumber,
+            'storename' : self.storename,           
+            'link': self.link,
+            'category': self.category
         }
     def serialize_just_name(self):
         return {
             'id': self.id,
             'firstname': self.firstname,
-            'lastname': self.lastname
+            'lastname': self.lastname,
+            'storename': self.storename,
+            'category': self.category
         }
 
 class Buyer(db.Model):
@@ -37,9 +50,11 @@ class Buyer(db.Model):
     id= db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
-    rut = db.Column(db.String(10), nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    rut = db.Column(db.String(12), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    favorite = db.relationship("Favorite", backref=db.backref("buyer", lazy = True))
+    payment = db.relationship("Payment", backref=db.backref("buyer", lazy = True))
 
     def __repr__(self):
         return "<Post %r>" % self.id
@@ -62,9 +77,162 @@ class Buyer(db.Model):
 
 
 
+
+class Sale(db.Model):
+    __tablename__ = 'sale'
+    id= db.Column(db.Integer, primary_key=True)
+    sellerID = db.Column(db.Integer, db.ForeignKey('seller.id'))
+    buyerID = db.Column(db.Integer, db.ForeignKey('buyer.id'))
+    productID = db.Column(db.Integer, db.ForeignKey('product.id'))
+    item_title = db.Column(db.String(50), nullable= False) 
+    item_price = db.Column(db.Integer, nullable= False)
+    seller = db.relationship("Seller", backref=db.backref("seller", lazy = True))
+    buyer = db.relationship("Buyer", backref=db.backref("buyer", lazy = True)) 
+    product = db.relationship("Product", backref=db.backref("product", lazy = True))
+     
+
+    def __repr__(self):
+        return "<Sale %r>" % self.id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'sellerID': self.sellerID,
+            'buyerID': self.buyerID,
+            'productID': self.productID,
+            'item_title': self.item_title,
+            'item_price': self.item_price
+        }
+    def serialize_just_name(self):
+        return {
+            'id': self.id,
+            'sellerID': self.sellerID
+        }
+
+
+class Product(db.Model):
+    __tablename__= 'product'
+    id= db.Column(db.Integer, primary_key=True)
+    sellerID = db.Column(db.Integer, db.ForeignKey('seller.id'))
+    storename = db.Column(db.String(30), nullable=False)
+    item_title = db.Column(db.String(50), nullable=False)
+    item_photo = db.Column(db.String(250), nullable=False)
+    item_description = db.Column(db.String(250), nullable=False)
+    item_stock = db.Column(db.Integer, nullable=False)
+    item_price = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return "<Product %r>" % self.id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'sellerID': self.sellerID,
+            'storename': self.storename,
+            'item_title': self.item_title,
+            'item_photo': self.item_photo,
+            'item_description': self.item_description,
+            'item_stock': self.item_stock,
+            'item_price': self.item_price
+        }
+
+    def serialize_just_name(self):
+        return {
+            'id': self.id,
+            'sellerID': self.sellerID,
+            'storename': self.storename,
+            'item_title': self.item_title
+        }
+
+
+class Favorite(db.Model):
+    __tablename__= 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    buyerID = db.Column(db.Integer, db.ForeignKey('buyer.id'))
+    sellerID = db.Column(db.Integer, db.ForeignKey('seller.id'))
+    storename = db.Column(db.String(30), nullable=False)
+    
+    
+
+    def __repr__(self):
+        return "<Favorite %r>" % self.id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'buyerID': self.buyerID,
+            'sellerID': self.sellerID,
+            'storename': self.storename
+        }
+    
+    def serialize_just_name(self):
+        return {
+            'id': self.id,
+            'storename': self.storename
+        }
+
+
+class Payment(db.Model):
+    __tablename__= 'payment'
+    id= db.Column(db.Integer, primary_key=True)
+    buyerID = db.Column(db.Integer, db.ForeignKey('buyer.id'))
+    debit = db.Column(db.Boolean, default=False, nullable=False)
+    credit = db.Column(db.Boolean, default=False, nullable=False)
+    transfer = db.Column(db.Boolean, default=False, nullable=False)
+
+    def __repr__(self):
+        return "<Payment %r>" % self.id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'buyerID': self.buyerID,
+            'debit': self.debit,
+            'credit': self.credit,
+            'transfer': self.transfer
+        }
+
+    def serialize_just_name(self):
+        return {
+            'id': self.id,
+            'debit': self.debit,
+            'credit': self.credit,
+            'transfer': self.transfer
+        }
+
+
+class Dispatch(db.Model):
+    __tablename__= 'dispatch'
+    id= db.Column(db.Integer, primary_key=True)
+    sellerID = db.Column(db.Integer, db.ForeignKey('seller.id'))
+    in_address = db.Column(db.String(100), nullable=False)
+    delivery = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return "<Dispatch %r>" % self.id
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'sellerID': self.sellerID,
+            'in_address': self.in_address,
+            'delivery': self.delivery
+        }
+
+    def serialize_just_name(self):
+        return {
+            'id': self.id,
+            'in_address': self.in_address,
+            'delivery': self.delivery
+        }
+
+
+
+
 class Balance(db.Model):
     __tablename__ = 'balance'
     id= db.Column(db.Integer, primary_key=True)
+    storename = db.Column(db.String(30), nullable=False)
     current_balance = db.Column(db.Integer)
     last_deposit = db.Column(db.Integer)
     last_withdraw = db.Column(db.Integer)
@@ -79,6 +247,8 @@ class Balance(db.Model):
     def serialize(self):
         return {
             'id': self.id,
+            'sellerID': self.sellerID,
+            'storename': self.storename,
             'current_balance': self.current_balance,
             'last_deposit': self.last_deposit,
             'last_withdraw': self.last_withdraw,
@@ -88,6 +258,9 @@ class Balance(db.Model):
     def serialize_just_name(self):
         return {
             'id': self.id,
-            'current_balance': self.current_balance
+            'storename': self.storename,
+            'current_balance': self.current_balance,
+            'last_deposit': self.last_deposit,
+            'last_withdraw': self.last_withdraw
             
         }
